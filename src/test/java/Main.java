f4f85369a402634042c45699e83c472d14ba0a60
RinuxLinux <rinux.rinux@gmail.com>
2018-09-08 16:39:34 +0200

    20180908 1639 worked on setup

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -3,78 +3,14 @@
  */
 package test.java;
 
-import java.io.File;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.sql.SQLException;
-import main.java.controller.db.SQLiteDBManager;
-
 public class Main {
 
     /**
      * Test pour first setup.
+     * @param args
      */
-    public String locpath = System.getenv("LOCALAPPDATA");
-    public String dirname = "FSP Reloaded";
-    public String dbname = "test01.db";
-
-    private static void testCreateTables() {
-
-        // SQLite connection string
-        String url = "jdbc:sqlite:" + DBpath + dbname;
-
-        // SQLite Statement for table creation
-        List<String> request = new ArrayList<>();
-
-        // TABLE designation ex-Recette
-        request.add("CREATE TABLE IF NOT EXISTS designation (\n"
-                + "   id int PRIMARY KEY NOT NULL,\n"
-                + "   nomPlat varchar(250),\n"
-                + "   regime varchar(250)\n"
-                + ");\n");
-
-
     public static void main(String[] args) {
-        String locpath = System.getenv("LOCALAPPDATA");
-        String dirname = "FSP Reloaded";
-        String dbname = "test01.db";
-
-        File dir0 = new File(locpath);
-
-        Path dpath = Paths.get(locpath, dirname);
-        Path fpath = Paths.get(locpath, dirname, dbname);
-
-        System.out.println(dpath);
-        System.out.println(fpath);
-        System.out.println(fpath.toFile().exists());
-
-        // check a few things ...
-        boolean create_locpath = !dir0.exists();
-        boolean create_db = !fpath.toFile().exists();
-        boolean create_wdir = !dpath.toFile().exists();
-        boolean locpath_isWriteable = dir0.canWrite();
-
-        // DB exists ?
-        System.out.println("Create LOCALAPPDATA ? " + create_locpath);
-        System.out.println("Create DB ? " + create_db);
-        System.out.println("Create FSP dir ? " + create_wdir);
-        System.out.println("Is LOCALAPPDATA writeable ? " + locpath_isWriteable);
-
-        //mkdir() doesnt throw exception but return 0 or 1
-        if (dpath.toFile().mkdirs()) {
-            System.out.println("Directories created in " + dpath);
-        } else {
-            System.out.println("Directories already exist.");
-        }
-
-        try {
-            //SQLiteDBManager session = new SQLiteDBManager();
-            SQLiteDBManager.createNewDatabase(fpath.toString());
-
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-
+        new testDBsetup().checkExistence();
     }
 
 }
--- src/test/java/testDBsetup.java
+++ src/test/java/testDBsetup.java
@@ -1,14 +1,128 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Test : hard-coding db setup.
  */
 package test.java;
+
+import java.io.File;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author re
  */
 public class testDBsetup {
-    
+
+    public String appDir = ".fsp_reloaded";
+    public String DBName = "test02.db";
+    public String parentDir = System.getenv("USERPROFILE");
+    public File installPath = Paths.get(parentDir, appDir).toFile();
+    public File DBFullpath = Paths.get(parentDir, appDir, DBName).toFile();
+
+    /**
+     * Check existence of install dir and db file.
+     */
+    public void checkExistence() {
+
+        // Install path exists ?
+        if (!installPath.exists()) {
+            System.out.println(installPath + " does not exist and will be created.");
+            installPath.mkdirs();
+        } else {
+            System.out.println("Install dir has been found at " + installPath + ".");
+        }
+
+        // DB file exists ?
+        if (!DBFullpath.exists()) {
+            System.out.println(DBName + " does not exists and will be created.");
+            // db is created when connected to. (?)
+        } else {
+            System.out.println("Database " + DBName + " has been found.");
+        }
+    }
+
+    /**
+     * Create table.
+     */
+    private static void createTables() {
+
+        // SQLite connection string
+        String mypath = "Z:/Dropbox/LABO-DBX/new-myebooks/";
+        String dbname = "180811.db";
+        String url = "jdbc:sqlite:" + mypath + dbname;
+
+        // SQLite Statement for table creation
+        List<String> request = new ArrayList<>();
+
+        request.add("CREATE TABLE IF NOT EXISTS warehouse (\n"
+                + "id integer PRIMARY KEY,\n"
+                + "name text NOT NULL,\n"
+                + "capacity real\n"
+                + ");\n");
+        request.add("CREATE TABLE IF NOT EXISTS material (\n"
+                + " id integer PRIMARY KEY,\n"
+                + " description text NOT NULL,\n"
+                + " picture blob\n"
+                + ");\n");
+        request.add("CREATE TABLE IF NOT EXISTS inventory (\n"
+                + " warehouse_id integer,\n"
+                + " material_id integer,\n"
+                + " qty real,\n"
+                + " PRIMARY KEY (warehouse_id, material_id),\n"
+                + " FOREIGN KEY (warehouse_id) REFERENCES warehouses (id),\n"
+                + " FOREIGN KEY (material_id) REFERENCES materials (id)\n"
+                + ");");
+
+        //System.out.println(sql);
+        try (Connection conn = DriverManager.getConnection(url);
+                Statement stmt = conn.createStatement()) {
+            // create new table = execute sql
+            //int count = 0;
+            for (int i = 0; i < request.size(); i++) {
+                stmt.execute(request.get(i));
+                System.out.println("La requete " + i + " a été exécutée.");
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /**
+     * Connect to the database.
+     *
+     * @return the Connection object
+     */
+    private Connection connect() {
+
+        // SQLite connection string
+        String dbpath = "Z:/Dropbox/LABO-DBX/new-myebooks/";
+        String dbname = "180811.db";
+        String url = "jdbc:sqlite:" + dbpath + dbname;
+        Connection conn = null;
+
+        try {
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+
+    /**
+     * Executing connect() and createTables().
+     */
+    public void setup() {
+
+        connect();
+        createTables();
+        System.out.println("Tables created.");
+
+    }
+
 }
